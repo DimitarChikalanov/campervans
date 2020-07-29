6f3cd993d09dafc0c02cb43b6e6eae344d2cb595
dimitar <dimitur.chikalanov@gmail.com>
2020-07-29 16:53:47 +0300

    Set sentry

--- src/main/java/com/campervan/service/impl/RentalServiceImpl.java
+++ src/main/java/com/campervan/service/impl/RentalServiceImpl.java
@@ -119,7 +119,7 @@ public class RentalServiceImpl implements RentalService {
   @Override
   public List getByCampervanID(long id) {
     String message = String.format("Service - getByCampervansID parameters" +id);
-    y.getContext().recordBreadcrumb(new BreadcrumbBuilder().setMessage(message).build());
+    Sentry.getContext().recordBreadcrumb(new BreadcrumbBuilder().setMessage(message).build());
     if (id<1){
       IllegalArgumentException e = new IllegalArgumentException("Invalid id");
       Sentry.capture(e);
